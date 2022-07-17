bdf103ee3819d4db9a2f51a9d7e2da2b87055c95
Raj Chauhan <74532684+racky7@users.noreply.github.com>
2022-07-17 17:40:46 +0000

    added validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 const User = require('../model/user')
+const {registerValidation, loginValidation} = require('../validation')
 
 router.post('/register', async (req,res)=>{
+  // Validate before creating user
+  const {error} = registerValidation(req.body)
+  if(error) return res.status(400).send(error.details[0].message)
   const user = new User({
     name: req.body.name,
     email: req.body.email,
@@ -10,9 +14,10 @@ router.post('/register', async (req,res)=>{
   try{
     const savedUser = await user.save()
     res.send(savedUser)
-  }catch(err){
+  }catch(err){ 
     res.status(400).send(err)
   }
+  
 })
 
 router.post('/login', (req,res)=>{
